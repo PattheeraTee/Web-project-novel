5edd7bdca4a6043f48f9db7b853cd0e4f187fd5a
PattheeraTee <pattheera.mwny@gmail.com>
2023-10-07 16:39:54 +0700

    navbar

---
+++ navbar.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navbarContainer = document.getElementById('navbar');
+
+    fetch('navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            navbarContainer.innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading the navbar:', error);
+        });
+});
